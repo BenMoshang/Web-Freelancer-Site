f8912431882aa1f5f173417d8b67333d0e3f326c
Ben Moshang <benmoshang@gmail.com>
2025-02-19 19:59:44 -0500

    Refactor: Optimize Hero Section, Typography, and Layout Styling

--- src/routes/overview-sections/Hero/HeroSection.svelte
+++ src/routes/overview-sections/Hero/HeroSection.svelte
@@ -38,6 +38,16 @@
 			secondHalf: 'DESIGN'
 		}
 	];
+	const ICONS = {
+		globe: {
+			viewBox: '0 0 512 512',
+			path: 'M352 256c0 22.2-1.2 43.6-3.3 64l-185.3 0c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64l185.3 0c2.2 20.4 3.3 41.8 3.3 64zm28.8-64l123.1 0c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64l-123.1 0c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32l-116.7 0c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0l-176.6 0c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0L18.6 160C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192l123.1 0c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64L8.1 320C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6l176.6 0c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352l116.7 0zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6l116.7 0z'
+		},
+		code: {
+			viewBox: '0 0 640 512',
+			path: 'M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z'
+		}
+	} as const;
 
 	/* Refs to DOM elements with proper typing */
 	let topPanel: HTMLElement;
@@ -53,17 +63,6 @@
 		force3D: true
 	} as const;
 
-	const ICONS = {
-		globe: {
-			viewBox: '0 0 512 512',
-			path: 'M352 256c0 22.2-1.2 43.6-3.3 64l-185.3 0c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64l185.3 0c2.2 20.4 3.3 41.8 3.3 64zm28.8-64l123.1 0c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64l-123.1 0c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32l-116.7 0c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0l-176.6 0c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0L18.6 160C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192l123.1 0c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64L8.1 320C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6l176.6 0c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352l116.7 0zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6l116.7 0z'
-		},
-		code: {
-			viewBox: '0 0 640 512',
-			path: 'M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z'
-		}
-	} as const;
-
 	function createDoorTimeline() {
 		return gsap.timeline({
 			scrollTrigger: {
@@ -71,6 +70,7 @@
 				start: 'top top',
 				end: '+=100%',
 				pin: true,
+
 				pinSpacing: true,
 				scrub: 2,
 				markers: false,
@@ -198,6 +198,8 @@
 	</svg>
 
 	<div class="hero__panel hero__panel--top" bind:this={topPanel} aria-hidden="true">
+		<h2 class="hero__subtitle">{heroInjectable.subtitle}</h2>
+
 		<h1 data-text={heroInjectable.titleFirstHalf} class="hero__title hero__title--top">
 			{heroInjectable.titleFirstHalf}
 		</h1>
@@ -207,7 +209,6 @@
 		<h1 data-text={heroInjectable.titleSecondHalf} class="hero__title hero__title--bottom">
 			{heroInjectable.titleSecondHalf}
 		</h1>
-		<h2 class="hero__subtitle">{heroInjectable.subtitle}</h2>
 		<ul class="hero__benefits" aria-label="Key Benefits">
 			{#each benefitsInjectable as benefit, index}
 				<li class="hero__benefits-item">
@@ -311,12 +312,27 @@
 		}
 
 		&__subtitle {
-			@extend %global__heading--h4;
-			margin-block-start: get-static-sp('lg');
+			@extend %global__label;
+			margin-block-end: get-responsive-sp('lg');
 			margin-inline: auto;
 			text-align: center;
 			text-transform: uppercase;
 			text-wrap: balance;
+			background: get-light-dark('600', '700', 0.38, 0.5);
+			@include apply-shadow('medium', true);
+			padding-block: get-responsive-sp('xs');
+			padding-inline: get-responsive-sp('sm');
+			border-radius: 5rem;
+			position: relative;
+
+			&::after {
+				content: '';
+				position: absolute;
+				inset: 0;
+				z-index: -1;
+				@include apply-shadow('low', false);
+				border-radius: 5rem;
+			}
 		}
 
 		&__benefits {
